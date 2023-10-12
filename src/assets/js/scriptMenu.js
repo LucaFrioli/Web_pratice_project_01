73843a867bb5cb12fd27559f15b5c36d069e5f95
LucaFrioli <lucafrioli7@gmail.com>
2023-10-12 17:31:36 -0300

    add cart page

--- src/assets/js/scriptMenu.js
+++ src/assets/js/scriptMenu.js
@@ -3,25 +3,22 @@ const items = document.querySelector(`menu#menuItems`);
 
 // Verificar e definir o estilo do menu com base na largura da janela
 function checkWindowSize() {
-    if (window.innerWidth >= 983) {
-      items.style.display = "flex";
-    } else {
-      items.style.display = "none";
-    }
+  if (window.innerWidth >= 983) {
+    items.style.display = "flex";
+  } else {
+    items.style.display = "none";
   }
-  
-  // Chama a função inicialmente e sempre que a janela for redimensionada
-  checkWindowSize();
+}
 
+// Chama a função inicialmente e sempre que a janela for redimensionada
+checkWindowSize();
 
-menuIcon.addEventListener(`click`, openMenu = () => {
-
-    if (items.style.display == `flex`) {
-        items.style.display = `none`;
-    } else {
-        items.style.display = `flex`;
-    }
-
+menuIcon.addEventListener(`click`, () => {
+  if (items.style.display == `flex`) {
+    items.style.display = `none`;
+  } else {
+    items.style.display = `flex`;
+  }
 });
 
-window.addEventListener("resize", checkWindowSize);//observa o redimencionamento da janela
+window.addEventListener("resize", checkWindowSize);
